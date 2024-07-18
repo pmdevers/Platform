7e483aa07b0c1d8b50ac0c3626c83eb51c6ba77b
Patrick Evers <patrick.evers@aah.nl>
2024-07-18 10:57:52 +0200

    wip

--- src/Hypotheek/Features/Leningen/Create.cs
+++ src/Hypotheek/Features/Leningen/Create.cs
@@ -1,5 +1,28 @@
-﻿namespace FinSecure.Platform.Hypotheek.Features.Leningen;
+﻿
+using FinSecure.Platform.Hypotheek.Domain.Leningen;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 
-public class Create
+namespace FinSecure.Platform.Hypotheek.Features.Leningen;
+
+public static class CreateLening
 {
+    public static void MapCreateLening(this IEndpointRouteBuilder builder)
+    {
+        builder.MapPost("/", HandleAsync);
+    }
+
+    private static async Task<Ok, BadRequest> HandleAsync(
+        [AsParameters] LeningenServices services,
+        [FromBody] CreateLeningRequest request
+        )
+    {
+
+        var lening = Lening.Create();
+
+
+
+    }
+
+    public record CreateLeningRequest(LeningId LeningId);
 }
--- src/Hypotheek/Features/Leningen/LeningenServices.cs
+++ src/Hypotheek/Features/Leningen/LeningenServices.cs
@@ -1,5 +1,10 @@
-﻿namespace FinSecure.Platform.Hypotheek.Features.Leningen;
+﻿using FinSecure.Platform.Common.Storage;
+using FinSecure.Platform.Hypotheek.Domain.Leningen;
 
-public class LeningenServices
+namespace FinSecure.Platform.Hypotheek.Features.Leningen;
+
+public class LeningenServices(AggregateManager<Lening, LeningId> leningManager, AggregateManager<Leningdeel, LeningdeelId> leningdeelManager)
 {
+    public AggregateManager<Lening, InkomenId> LeningManager { get; } = leningManager;
+    public AggregateManager<Leningdeel, InkomenId> LeningdeelManager { get; } = leningdeelManager;
 }
