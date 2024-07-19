dd9777d6d33bdadeca55f20193f49540f9aac988
Patrick Evers <patrick.evers@aah.nl>
2024-07-19 13:56:32 +0200

    Do minimal Test

--- test/Hypotheek.Tests/UnitTest.cs
+++ test/Hypotheek.Tests/UnitTest.cs
@@ -1,10 +1,16 @@
-﻿namespace Hypotheek.Tests;
+﻿using FinSecure.Platform.Hypotheek.Domain.Aanvragen;
+
+namespace Hypotheek.Tests;
 
 public class UnitTest
 {
     [Fact]
     public void Test()
     {
-        Assert.True(true);
+        var aanvraag = Aanvraag.Create(AanvraagId.Next());
+
+        var events = aanvraag.GetUncommittedEvents();
+
+        Assert.Single(events);
     }
 }
