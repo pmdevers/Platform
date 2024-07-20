421d13e4a2e76b7cf115e1f7b20dcfb0d0c77870
Patrick Evers <pmdevers@gmail.com>
2024-07-20 02:46:31 +0200

    Complete build

--- src/HypotheekAdviseur/Features/Core/Landing.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Http.HttpResults;
-
-namespace FinSecure.Platform.HypotheekAdviseur.Features.Core;
-
-public static class Landing
-{
-    public static void MapLandingPage(this IEndpointRouteBuilder builder) 
-        => builder.MapGet("/", () => new RazorComponentResult<MainPage>());
-}
